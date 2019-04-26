e7cb7174417d0f143c7ebdca4e6d8fd971039040
DANNY BERNARD <danny.bernard@hotmail.com>
2019-04-26 05:22:25 -0400

    avazando

--- app.js
+++ app.js
@@ -1,34 +1,64 @@
-class product{
-    constructor(name,precio,year){
-    this.name =name;
-    this.price=price;
-    this.year=year;
+class Product {
+    constructor(name, price, year) {
+        this.name = name;
+        this.price = price;
+        this.year = year;
     }
 
-    
+
 }
-class UI{
-      addProduct(){
+class UI {
+    addProduct(product) {
+        const productList = document.getElementById('product-list');
+        const element = document.createElement('div');
+        element.innerHTML = `
+            <div class = "card text-center mb-4>" >
+                <div class="card-body">
+                    <strong>product Name</strong>: ${product.name}
+                    <strong>product Price</strong>: ${product.price}
+                    <strong>product year</strong>: ${product.year}
+                    <a href="#" class="btn btn-danger" name="delete"> Delete</a>
+                </div>
+        </div >
+          `;
+        productList.appendChild(element);
+    }
 
-      }  
-      deleteProduct(){
+    restfrom(){
+        document.getElementById('product-form').reset();
+    }
 
-      }
+    deleteProduct() {
+    
+    }
 
-    showmessage(){
+    showmessage() {
 
     }
 }
 
 //dome event
-document.getElementById('product-form').addEventListener('submit',function(e){
-   const name = document.getElementById('name').value;
-   const price= document.getElementById('price').value;
-   const year = document.getElementById('year').value;
+document.getElementById('product-form').addEventListener('submit', function (e) {
+    const name = document.getElementById('name').value;
+    const price = document.getElementById('price').value;
+    const year = document.getElementById('year').value;
 
-   console.log(name,price,year);
+    console.log(name, price, year);
 
-   e.preventDefault();
+    const product = new Product(name, price, year);
 
-})
+    const ui = new UI();
+  
+
+    ui.addProduct(product);
+    ui.restfrom();
+    e.preventDefault();
+
+
+
+});
+
+document.getElementById('product-list').addEventListener('click',function(e){
+ 
+});
 
